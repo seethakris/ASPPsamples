22d953fae3ba8fb1e7fb96b4c8fda30aedcb9c8f
seethakris <seethakris@gmail.com>
2018-01-03 22:26:50 +0530

    Updated comments

--- Exercise2_webcam_getoneframe.py
+++ Exercise2_webcam_getoneframe.py
@@ -1,4 +1,6 @@
-## Sample program that acquires a single image from the webcam
+"""
+Sample program that acquires a single image from the webcam and plots image intensity
+"""
 
 import cv2
 import numpy as np
@@ -7,32 +9,49 @@ import matplotlib.pyplot as plt
 
 
 def display_images(method='numpy'):
-    vc = cv2.VideoCapture(0)
-    # Generate just one image
-    g = stream_frames(video_capture=vc)
-    bgrframe = next(g)  # Generate an image from the webcam
+    """
+    Main function to call
+    Obtains a single image from the function stream_frames, plots and exits
+    :param method: Method to convert bgr to rgb - opencv or numpy
+    """
+    vc = cv2.VideoCapture(0)  # Open webcam using opencv library
 
-    ## The video captured is in format bgr but matplotlib requires rgb
-    starttime = time.time()
+    # Yield one image from generator function stream_frames
+    g = stream_frames(video_capture=vc)
+    bgrframe = next(g)
+
+    # The video captured by opencv is in format bgr (blue-green-red) but matplotlib requires rgb
+    starttime = time.time()  # calculate elapsed time for conversion
     rgbframe = convert_bgr_to_rgb(bgrframe, method=method)
     print(f'Time to convert using {method} = {(time.time() - starttime):0.4f} seconds')
 
-    # Plot frame
+    # Plot frame and intensity
     figure, ax = plt.subplots(1, 2, figsize=(10, 5))
-    intensity = check_imageintensity(image=rgbframe[:, :, :])  # Try plotting different
+    intensity = check_imageintensity(image=rgbframe[:, :, :])
     plot_image_and_brightness(axis=ax, image=rgbframe[:, :, :], imageintensity=intensity)
 
-    g.close()
+    g.close()  # Call GeneratorExit for cleanup
 
 
 def check_imageintensity(image):
-    # This function acquires image intensity
+    """
+    This function acquires image intensity
+    :param image: rgb image
+    :return: imageintensity : average intensity of image
+    """
+
     imageintensity = np.mean(image)
     return imageintensity
 
 
 def convert_bgr_to_rgb(bgr, method='numpy'):
-    # This function converts image to RGB
+    """
+    This function converts image to RGB
+    :param  bgr: bgr image
+            method: specify method to convert bgr to rgb - opencv or numpy indexing
+    :return: rgb image
+    """
+
     if method == 'numpy':
         rgb = bgr  # Use numpy indexing to convert image to rgb
         # Solution:
@@ -45,6 +64,12 @@ def convert_bgr_to_rgb(bgr, method='numpy'):
 
 
 def plot_image_and_brightness(axis, image, imageintensity):
+    """
+    This function plots image and intensity of image
+    :param  axis: figure axis for plotting
+            image: rgb image
+            imageintensity: intensity of image
+    """
     # Plot webcam image
     axis[0].imshow(image)
     axis[0].axis('off')
@@ -56,7 +81,11 @@ def plot_image_and_brightness(axis, image, imageintensity):
 
 
 def stream_frames(video_capture):
-    # This function acquires image
+    """
+    Generator function that captures image from webcam
+    :param  video_capture: the video capture object from opencv
+    :yield  frame: image from webcam
+    """
     try:
         while True:
             _, frame = video_capture.read()
@@ -67,4 +96,3 @@ def stream_frames(video_capture):
 
 
 display_images(method='opencv')
-
